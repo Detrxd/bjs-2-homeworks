d5a4ad1d83492933f86cf8c64a09edb3d3755bfd
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-12-10 17:22:11 +0300

    Rework task#2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -17,28 +17,23 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   if (isNaN(percent)) {
-    console.log(`Параметр "Процент" содержит неправильное значение ${percent}`)
+    return(`Параметр "Процент" содержит неправильное значение "${percent}"`)
   }
   if (isNaN(contribution)) {
-    console.log(`Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`)
+    return(`Параметр "Сумма первоначального взноса" содержит неправильное значение "${contribution}"`)
   }
   if (isNaN(amount)) {
-    console.log(`Параметр "Сумма кредита" содержит неправильное значение ${amount}`)
+    return(`Параметр "Сумма кредита" содержит неправильное значение "${amount}"`)
   }
 
   let bodyAmount = amount - contribution;
 
   let toDay = new Date();
 
-  let currentDayOfMonth = date.getUTCMonth() - toDay.getUTCMonth() + (date.getFullYear() - toDay.getFullYear() * 12);
+  let currentDayOfMonth = date.getUTCMonth() - toDay.getUTCMonth() + (date.getFullYear() - toDay.getFullYear()) * 12;
 
   let weeklyPayPerMonth = bodyAmount * (percent / 12 / 100 + (percent / 12 / 100) / (((1+ percent / 12 /100)**currentDayOfMonth)-1));
 
   totalAmount = Number((weeklyPayPerMonth * currentDayOfMonth).toFixed(2));
 
-  console.log(totalAmount);
-
   return totalAmount;
-}
-
-// s * (percent / 12 / 100 + (percent / 12 / 100) / (((1 + percent / 12 / 100) ** month) - 1));
