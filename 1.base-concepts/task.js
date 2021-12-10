645d3d20c72ba5c4e730de44afb1d0f649d5c44d
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-12-10 16:57:41 +0300

    Ending all task from HW #1 (+ 2 Task)

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,44 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  let d= b ** 2 - 4 * a *c;
+  let d = b ** 2 - 4 * a * c;
   if (d == 0) {
-	  arr.push(-b / (2 * a));
-  } 
-  if (d > 0) {
-	  arr.push((-b + Math.sqrt(d)) / (2 * a));
-	  arr.push((-b - Math.sqrt(d)) / (2 * a));  
+    arr.push(-b / (2 * a));
   }
-  return arr; 
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    console.log(`Параметр "Процент" содержит неправильное значение ${percent}`)
+  }
+  if (isNaN(contribution)) {
+    console.log(`Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`)
+  }
+  if (isNaN(amount)) {
+    console.log(`Параметр "Сумма кредита" содержит неправильное значение ${amount}`)
+  }
+
+  let bodyAmount = amount - contribution;
+
+  let toDay = new Date();
+
+  let currentDayOfMonth = date.getUTCMonth() - toDay.getUTCMonth() + (date.getFullYear() - toDay.getFullYear() * 12);
+
+  let weeklyPayPerMonth = bodyAmount * (percent / 12 / 100 + (percent / 12 / 100) / (((1+ percent / 12 /100)**currentDayOfMonth)-1));
+
+  totalAmount = Number((weeklyPayPerMonth * currentDayOfMonth).toFixed(2));
+
+  console.log(totalAmount);
 
   return totalAmount;
 }
+
+// s * (percent / 12 / 100 + (percent / 12 / 100) / (((1 + percent / 12 / 100) ** month) - 1));
