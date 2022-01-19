523503ebec1f4dd3716df5c6e6e5c73ffd5320ce
Ilya Filimonov <ilyafil1996@yandex.ru>
2022-01-19 19:48:35 +0300

    push task 1, 2 and 3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,46 @@
 // Задание 1
+
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+  avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
 
-  return { min: min, max: max, avg: avg };
+  return { min: min, max: max, avg: +(sum / arr.length).toFixed(2) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let result = getArrayParams(arr);
+  return Math.abs(result.max - result.min);
 }
