e58448c3b225927a2357bc525af48d6331bb5c3e
Ilya Filimonov <ilyafil1996@yandex.ru>
2022-05-26 15:02:56 +0300

    Refactoring a HW

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -27,8 +27,6 @@ Student.prototype.addMarks = function (...points) {
 }
 
 Student.prototype.getAverage = function () {
-  console.log(this);
-  
   let initialValue = 0;
   let sumAllValues = this.marks.reduce((previousValue, currentValue) =>
     previousValue + currentValue, initialValue);
@@ -42,34 +40,18 @@ Student.prototype.exclude = function (reason) {
 }
 
 
-// let generalMark = 0;
-// for (index = 0; index < this.marks.length; index++) {
-//   generalMark = generalMark + this.marks[index];
-// }
-// return generalMark / this.marks.length;
-
-// let initialValue = 0;
-//   let sumAllValues = this.marks.reduce((previousValue, currentValue) =>
-//     previousValue + currentValue, initialValue);
-// return sumAllValues/this.marks.length;
-
-
-// let student2 = new Student("Masha", "Female", 30);
-// student2.setSubject("Geography")
-// student2.addMark(4);
-// student2.addMark(3);
-// student2.addMark(5);
-// student2.addMarks(2,3,3);
-// // console.log(student2.getAverage());
-
-// console.log(student2);
-
-
-let student1 = new Student("Misha", "Male", 50);
-student1.setSubject("Biology");
-student1.addMarks(2, 3, 2);
-// student1.exclude('low grades');
+let student1 = new Student("Masha", "Female", 30);
+student1.setSubject("Geography")
+student1.addMark(4);
+student1.addMark(3);
+student1.addMark(5);
+student1.addMarks(2, 3, 3);
 console.log(student1.getAverage());
-
 console.log(student1);
+
+let student2 = new Student("Misha", "Male", 40);
+student2.setSubject("Biology")
+student2.addMarks(2, 3, 3);
+student2.exclude('low grades');
+console.log(student2);
 
