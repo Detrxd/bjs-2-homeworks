fc04203cbcb0f957d6588fafc96495944702a29a
Ilya Filimonov <ilyafil1996@yandex.ru>
2022-05-26 14:51:56 +0300

    Finished HW

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,36 +1,75 @@
 function Student(name, gender, age) {
-   this.name = name;
-   this.gender = gender;
-   this.age = age;
-   let marks;
-   let subject;
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+  let excluded;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subjectName = Student.subject; 
+  this.subject = subjectName;
 }
-
 
 Student.prototype.addMark = function (mark) {
-  if(this.marks === undefined){ 
+  if (this.marks === undefined) {
+    this.marks = mark;
+  } else {
+    this.marks.push(mark);
+  }
+}
 
-    } else {
-      // добавить вторую и последующие оценки в массив
-    } 
+Student.prototype.addMarks = function (...points) {
+  if (this.marks === undefined) {
+    this.marks = points;
+  } else {
+    this.marks.push(...points);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  console.log(this);
+  
+  let initialValue = 0;
+  let sumAllValues = this.marks.reduce((previousValue, currentValue) =>
+    previousValue + currentValue, initialValue);
+  return sumAllValues / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
 
 
-Student.prototype.setSubject = function (subjectName) {
-  this.subjectName = Student.subject; 
-}
+// let generalMark = 0;
+// for (index = 0; index < this.marks.length; index++) {
+//   generalMark = generalMark + this.marks[index];
+// }
+// return generalMark / this.marks.length;
+
+// let initialValue = 0;
+//   let sumAllValues = this.marks.reduce((previousValue, currentValue) =>
+//     previousValue + currentValue, initialValue);
+// return sumAllValues/this.marks.length;
 
 
-Student.prototype.setSubject = function (subjectName) {
-  this.subjectName = Student.subject; 
-}
+// let student2 = new Student("Masha", "Female", 30);
+// student2.setSubject("Geography")
+// student2.addMark(4);
+// student2.addMark(3);
+// student2.addMark(5);
+// student2.addMarks(2,3,3);
+// // console.log(student2.getAverage());
+
+// console.log(student2);
 
 
-Student.prototype.setSubject = function (subjectName) {
-  this.subjectName = Student.subject; 
-}
+let student1 = new Student("Misha", "Male", 50);
+student1.setSubject("Biology");
+student1.addMarks(2, 3, 2);
+// student1.exclude('low grades');
+console.log(student1.getAverage());
+
+console.log(student1);
+
