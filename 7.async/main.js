ebe1ada980dfec7568f766e7c08a42cc8139b8e5
Ilya <ilyafil96@gmail.com>
2022-06-19 18:27:38 +0300

    Добавление main.js, создание тестового сценария

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,11 +1,22 @@
-function TestCase() {
-    let attention = new AlarmClock();
-    attention.addClock("16:30", () => console.log('Звонок установлен на'), 1)
+function testCase() {
+    clock = new AlarmClock();
+    clock.addClock("16:45", () => console.log(`Добавление звонка`), 1);
 
-    attention.addClock("16:50", () => {
-        console.log('Пора вставать'),
-            attention.clearAlarms(2)
+    clock.addClock("16:46", () => {
+        console.log(`Повторное добавление звонка`);
+        clock.removeClock(2);
     }, 2);
+
+    clock.addClock("16:47", () => {
+        console.log(`Добавление-удаление будильника`);
+        clock.stop();
+        clock.clearAlarms();
+        clock.printAlarms();
+    }, 3);
+
+    clock.printAlarms();
+
+    clock.start();
 }
 
-console.log(TestCase());
+testCase();
