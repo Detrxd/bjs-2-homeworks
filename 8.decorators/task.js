d67895c3872a1d45ed06903e287449eef400caef
Detrxd <ilyafil96@gmail.com>
2022-07-14 16:11:22 +0300

    Init

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,19 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  return function (...args) {
+    hash = args.join(',');
+
+    if (hash in cache) {
+      console.log("Из кэша: " + cache[hash]);
+    } else if (Object.keys(cache).length === 5) {
+      delete cache[Object.keys(cache)[0]];
+      cache[hash] = func.apply(this, args);
+      console.log("Вычисляем: " + cache[hash]);
+    } else {
+      cache[hash] = func.apply(this, args);
+      console.log("Вычисляем: " + cache[hash]);
+    }
+  };
 }
 
 
@@ -10,3 +24,4 @@ function debounceDecoratorNew(func) {
 function debounceDecorator2(func) {
   // Ваш код
 }
+
